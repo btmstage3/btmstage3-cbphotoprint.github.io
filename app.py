f08b018180e69f1beb5c179f69ed6f8ff453470e
btmstage3 <73518245+btmstage3@users.noreply.github.com>
2023-03-15 16:33:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request
+import subprocess
 
 app = Flask(__name__)
 
-@app.route('/print_photos', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        num_photos = request.form['num_photos']
-        # Call your Python function to generate the passport photos
-        return render_template('results.html', num_photos=num_photos)
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/print_photos', methods=['POST'])
+def execute():
+    # Call your Python file using subprocess
+    subprocess.call(['python', 'print-photos.py'])
+    return 'Python file executed successfully!'
+
+
+@app.route('/print_photos', methods=['POST'])
+def print_photos():
+    num_photos = request.form['num_photos']
+    # Call your Python function to generate the passport photos
+    return render_template('results.html', num_photos=num_photos)
 
 if __name__ == '__main__':
     app.run(debug=True)
