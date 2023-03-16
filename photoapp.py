5df8fe160c12a7554fb5074b111e026dcc92757d
btmstage3 <73518245+btmstage3@users.noreply.github.com>
2023-03-16 13:31:47 +0530

    Add files via upload

--- photoapp.py
+++ photoapp.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
-from PIL import Image
+from flask import Flask, render_template, request, redirect, url_for
 import tkinter as tk
 from tkinter import filedialog
+from PIL import Image, ImageOps
 
 app = Flask(__name__, template_folder='')
 
@@ -82,6 +82,40 @@ def create_passport_photo_sheet(num_photos: int, output_format: str):
 
                 a4_image.paste(padded_image, (x, y))
 
+    elif num_photos == 20:
+        # Calculate the total available space on the A4 sheet
+        total_width = a4_size[0] - 2 * 100  # Subtract the left and right margins
+        total_height = a4_size[1] - 2 * 100  # Subtract the top and bottom margins
+
+        # Calculate the total width and height of the photos based on the number of photos
+        num_rows = num_photos // 4
+        num_cols = 4
+        if num_photos == 20:
+            num_rows = 5
+        photo_width = num_cols * photo_size[0] + (num_cols - 1) * 100
+        photo_height = num_rows * photo_size[1] + (num_rows - 1) * 50
+
+        # Calculate the left and right margins for centering the photo grid horizontally
+        left_margin = (total_width - photo_width) // 2 + 100
+        right_margin = total_width - left_margin - photo_width
+
+        # Calculate the top margin for centering the photo grid vertically
+        top_margin = (total_height - photo_height) // 2 + 100
+
+        # Paste the input image onto the A4 image in a grid
+        for row in range(num_rows):
+            for col in range(num_cols):
+                x = left_margin + col * (photo_size[0] + 100)
+                y = top_margin + row * (photo_size[1] + 50)
+                resized_image = input_image.resize(photo_size)
+
+                # Create a new image with a black margin
+                padded_size = (photo_size[0] + margin_size * 2, photo_size[1] + margin_size * 2)
+                padded_image = Image.new("RGB", padded_size, margin_color)
+                padded_image.paste(resized_image, (margin_size, margin_size))
+
+                a4_image.paste(padded_image, (x, y))
+
 
     else:
         print("Invalid number of photos selected. Please select 8, 12, or 16.")
@@ -120,6 +154,9 @@ def create_passport_photo_sheet(num_photos: int, output_format: str):
         # Save the output as an image file
         a4_image.save(output_file_path)
 
+  # redirect to the initial page
+    return redirect(url_for('index'))
+
 @app.route('/print_photos', methods=['POST'])
 def print_photos_post():
     num_photos = request.form['num_photos']
@@ -134,13 +171,13 @@ def print_photos_post():
 
     # Use the retrieved value to perform the necessary logic
     # ...
-    return 'Photos printed: ' + num_photos + "\t\t\t" + output_format
+    # return 'Photos printed: ' + num_photos + "\t\t\t" + output_format
+    # redirect to the initial page
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     app.run(host='localhost', port=9874, debug=True)
 
-    num_photos = request.form['num_photos']
-    output_format: str = request.form['output_format']
 
 
